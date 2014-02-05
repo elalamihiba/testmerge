f99e0762404930a68de99937dc072e656ea924c8
elalamihiba <hibatallahelalami@gmail.com>
2014-02-05 13:15:27 +0000

    first

---
+++ premier.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+		int a,b,c;
+		c=a+b;
+		System.out.println("%d",c);
+    }
+
+}
